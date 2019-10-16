933ad4331f0b85d2dd12e8f9b482789516712895
care526 <710783534@qq.com>
2019-10-17 02:13:22 +0800

    fix

---
+++ 其他/codeStyle.js
@@ -0,0 +1,19 @@
+import { A, B } from C.js
+
+// 命名
+var a = 0
+var a = b
+var _this = this;
+var shopName = 'care'
+var care = [1, 2, 3, 4]
+
+// 语句
+if (true) {}
+for (let i = 0; i < 4; i++) {
+  // 两个空格的Tab
+}
+function name(args) {
+  // XXX
+}
+
+true ? a = 3 : ''
