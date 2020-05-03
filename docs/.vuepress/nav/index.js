9de3b159996cb67b9b8332766528022cb90d6f75
care526Lj <710783534@qq.com>
2020-05-03 11:33:31 +0800

    a

--- docs/.vuepress/nav/index.js
+++ docs/.vuepress/nav/index.js
@@ -10,7 +10,7 @@ module.exports = [
                     { text: 'CSS', link: '/CSS/qa' },
                     { text: 'SVG', link: '/SVG/all' },
                     { text: 'HTML', link: '/HTML/HTML' },
-                    { text: 'Node', link: '/' },
+                    { text: 'Node', link: '/Node/fs' },
                 ] 
             },
             {
@@ -39,61 +39,61 @@ module.exports = [
     { text: 'Linux', link: '/linux/vim' },
     { text: 'Git', link: '/Git/branch' },
 
-    {
-        text: '编程其他', 
-        items: [
-            { text: '编程思想', link: '/programmingThought/function' },
-            { text: '数据结构', link: '/programmingThought/function' },
-        ]
-    },
-    {
-        text: '书籍',
-        items: [
-            {
-                text: '技术', 
-                items: [
-                    { text: 'Webpack', link: '/' },
-                ]
-            },
-            {
-                text: '非技术', 
-                items: [
-                    { text: 'Webpack', link: '/' },
-                ]
-            }
-        ] 
-    },
-    {
-        text: '影视',
-        items: [
-            {
-                text: '电视剧', 
-                items: [
-                    { text: 'Webpack', link: '/' },
-                ]
-            },
-            {
-                text: '综艺', 
-                items: [
-                    { text: 'Webpack', link: '/' },
-                ]
-            },
-            {
-                text: '纪录片', 
-                items: [
-                    { text: 'Webpack', link: '/' },
-                ]
-            }
-        ] 
-    },
-    {
-        text: '杂记',
-        items: [
-            { text: '2020', link: '/2020/5' },
-        ]
-    },
-    { text: '经济', link: '/' },
-    { text: '心理', link: '/' },
-    { text: '历史', link: '/' },
+    // {
+    //     text: '编程其他', 
+    //     items: [
+    //         { text: '编程思想', link: '/programmingThought/function' },
+    //         { text: '数据结构', link: '/programmingThought/function' },
+    //     ]
+    // },
+    // {
+    //     text: '书籍',
+    //     items: [
+    //         {
+    //             text: '技术', 
+    //             items: [
+    //                 { text: 'Webpack', link: '/' },
+    //             ]
+    //         },
+    //         {
+    //             text: '非技术', 
+    //             items: [
+    //                 { text: 'Webpack', link: '/' },
+    //             ]
+    //         }
+    //     ] 
+    // },
+    // {
+    //     text: '影视',
+    //     items: [
+    //         {
+    //             text: '电视剧', 
+    //             items: [
+    //                 { text: 'Webpack', link: '/' },
+    //             ]
+    //         },
+    //         {
+    //             text: '综艺', 
+    //             items: [
+    //                 { text: 'Webpack', link: '/' },
+    //             ]
+    //         },
+    //         {
+    //             text: '纪录片', 
+    //             items: [
+    //                 { text: 'Webpack', link: '/' },
+    //             ]
+    //         }
+    //     ] 
+    // },
+    // {
+    //     text: '杂记',
+    //     items: [
+    //         { text: '2020', link: '/2020/5' },
+    //     ]
+    // },
+    // { text: '经济', link: '/' },
+    // { text: '心理', link: '/' },
+    // { text: '历史', link: '/' },
     { text: 'Home', link: '/' }
 ]
