e7012dfcd075fb82f25dd21a07925b86a48b2e8a
care526Lj <710783534@qq.com>
2020-05-08 15:06:01 +0800

    a

--- docs/.vuepress/sidebar/index.js
+++ docs/.vuepress/sidebar/index.js
@@ -1,23 +1,50 @@
 const path = require('path')
 const fs = require('fs')
 
-const Items = fs.readdirSync(path.resolve(__dirname, '../../'))
-                .filter(rootPath => !['.vuepress', 'README.md'].includes(rootPath))
+const docsPath = path.resolve(__dirname, '../../')
+
+const Items = fs.readdirSync(docsPath)
+                .filter(child => !['.vuepress', 'README.md'].includes(child))
+
+
+// 获取目录下的md文件
+function findMdChild(dir, dirname) {
+    return {
+        title: dirname,
+        collapsable: false,
+        children: fs.readdirSync(dir)
+                    .filter(fileName => fileName.indexOf('.md') > -1)
+                    .map(fileName => fileName.slice(0, -3))
+    }
+}
+
+function findDirChild(dir) {
+    return fs.readdirSync(dir)
+            .filter(fileName => fs.statSync(path.resolve(dir, fileName)).isDirectory() && fileName !== 'images')
+}
+
+function setGroups(targetDir, targetDirName) {
+    let groups = []
+
+    groups[0] = findMdChild(...arguments)
+    const targetChildDir = findDirChild(...arguments)
+    if (targetChildDir.length > 0) {
+        targetChildDir.forEach((dirname, index) => {
+            groups[index + 1] = findMdChild(
+                path.resolve(targetDir, dirname),
+                dirname
+            )
+        })
+    }
+
+    console.log(groups)
+    return groups
+}
 
 let Routers = {}
 
-function findChild(dirName) {
-    return fs.readdirSync(path.resolve(__dirname, `../../${dirName}`))
-            .filter(fileName => fileName.indexOf('.md') > -1)
-            .map(fileName => fileName.slice(0, -3))
-}
-
 Items.forEach(rootPath => {
-    Routers[`/${rootPath}/`] = [{
-        title: rootPath,
-        collapsable: false,
-        children: findChild(rootPath)
-    }]
+    Routers[`/${rootPath}/`] = setGroups(path.join(docsPath, rootPath), rootPath)
 })
 
 module.exports = Routers
