7bffd330026a2a3c3d1237099fc9c780c40e65d0
care526Lj <710783534@qq.com>
2020-05-24 13:18:30 +0800

    a

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,6 @@ const nav = require('./nav')
 const sidebar = require('./sidebar')
 const plugins = require('./plugins')
 
-console.log(plugins)
-
 module.exports = {
     title: "care's blog",
     description: 'care的知识笔记',
@@ -21,6 +19,18 @@ module.exports = {
         
         plugins,
 
-        lastUpdated: 'Last Updated', // string | boolean
+        lastUpdated: true,
+    },
+
+    extraWatchFiles: [
+        '../.vuepress'
+    ],
+
+    dest: '../dist',
+
+    port: 4000,
+
+    markdown: {
+        // lineNumbers: true
     }
 }
