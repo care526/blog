f865c7ad0c14e8bcd8cf4229a97cb22ebe4db9f1
care526 <710783534@qq.com>
2020-10-17 04:50:25 +0800

    a

--- bash/rename.js
+++ bash/rename.js
@@ -5,7 +5,47 @@ const { exec } = require('child_process')
 // 待处理目录
 const RootPath = 'docs'
 // 所有的文件集合
-const allFileName = {}
+const allFileTree = {
+    id: 'care的知识体系',
+    children: []
+}
+setAllFileTree = (type, pathName) => {
+    if (pathName.indexOf('README') > -1) return
+
+    const arr = pathName.slice(5).split('/')
+    arr.unshift(allFileTree.children)
+    if (type === 'dir') {
+        arr.reduce((root, parentNode) => {
+            if (!root) return
+            const node = {
+                id: parentNode,
+                children: []
+            }
+            const Nodes = root.filter(i => i.id === parentNode)
+            if (Nodes.length) {
+                return Nodes[0].children
+            } else {
+                root.push(node)
+                return node.children
+            }
+        })
+    }
+    if (type === 'md') {
+        console.log('-------')
+        arr.reduce((root, childNode) => {
+            if (!root) return
+            if (!childNode.indexOf('.md') > -1) {
+                if (root.filter(i => i.id === childNode).length) {
+                    return root.filter(i => i.id === childNode)[0].children
+                } else {
+                    root.push({
+                        id: childNode.slice(0, -3)
+                    })
+                }
+            }
+        })
+    }
+}
 // 处理排除的目录
 const exclude = filename => filename !== '.vuepress'
 // 重命名md文档
@@ -27,12 +67,7 @@ function moveMd(mdPath) {
         if (err) console.error(err, mdPath)
 
         const newName = reMdFileName()
-        const title = stdout.slice(2).replace('\n', '')
-        if (allFileName[title]) {
-            console.log(title)
-        } else {
-            allFileName[title] = newName.replace('md', 'html')
-        }
+        setAllFileTree('md', mdPath)
         exec(`cp ${mdPath} ./docsWillBuild/${newName}`)
     })
     
@@ -49,6 +84,7 @@ function dealUnitDir(dirPath) {
         if (filename.indexOf('.md') > -1) {
             return moveMd(filePath)
         }
+        setAllFileTree('dir', filePath)
         dealUnitDir(filePath)
     })
 }
@@ -56,9 +92,8 @@ function dealUnitDir(dirPath) {
 dealUnitDir(RootPath)
 
 setTimeout(() => {
-    fs.writeFile('./docsWillBuild/.vuepress/data.json', JSON.stringify(allFileName), 'utf-8', err => {
+    fs.writeFile('./docsWillBuild/.vuepress/data.json', JSON.stringify(allFileTree), 'utf-8', err => {
         if (err) console.error(err)
         console.log('写入成功')
     })
-    console.log('总md文件数目:' + Object.keys(allFileName).length)
 }, 5 * 1000)
