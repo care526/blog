314f21667f73494a11b42db660987e778b3c50ff
care526 <710783534@qq.com>
2020-11-05 10:04:58 +0800

    2020-11-05

---
+++ bash/push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 复制博客展示项目的git到打包目录
+rm -rf ./docsWillBuild/.git
+cp -r ../care526.github.io/.git ./docsWillBuild/.git
+
+time=$(date "+%Y-%m-%d")
+
+git add .
+git commit -m $time
+# git push
