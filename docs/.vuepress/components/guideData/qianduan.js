b2637c0d901fb3c0352513e62ad5f418b5c6c4ce
care526 <710783534@qq.com>
2021-03-17 20:03:46 +0800

    a

---
+++ docs/.vuepress/components/guideData/qianduan.js
@@ -0,0 +1,96 @@
+module.exports = { text: "前端", children: [
+    { text: "社区", children: [
+        { text: "掘金", link: "https://juejin.im/" },
+        { text: "思否", link: "https://segmentfault.com/" },
+        { text: "前端乱炖", link: "http://www.html-js.com/" },
+        { text: "云栖社区", link: "https://yq.aliyun.com/" },
+        { text: "W3school", link: "https://www.w3school.com.cn/index.html" },
+        { text: "coderplanets", link: "https://coderplanets.com/home/posts" },
+        { text: "Web前端导航", link: "http://www.alloyteam.com/nav/" },
+        { text: "力扣", link: "https://leetcode-cn.com/" },
+        { text: "HelloGitHub", link: "https://hellogithub.com/" },
+        { text: "编程导航", link: "https://www.code-nav.cn/recommend" },
+    ]},
+    { text: "好的第三方项目源码", children: [
+        { text: "github", children: [
+            { text: "网易云NodeJS API", link: "https://binaryify.github.io/NeteaseCloudMusicApi/#/" },
+            { text: '前端开发知识结构', link: 'https://github.com/JacksonTian/fks' },
+            { text: '正则表达式', link: 'https://github.com/ziishaned/learn-regex/blob/master/translations/README-cn.md' },
+        ]},
+    ]},
+    { text: "个人博客", children: [
+        { text: "前端", children: [
+            { text: "fouber", link: "https://github.com/fouber/blog" },
+        ]},
+    ]},
+    { text: "CSS", children: [
+        { text: "CSS速查总表", link: "https://www.tslang.cn/docs/home.html" },
+        { text: "Sass", link: "https://www.sass.hk/" },
+        { text: "Less", link: "http://lesscss.cn/" },
+        { text: "Stylus", link: "https://stylus.bootcss.com/" },
+        { text: "兼容性", link: "https://caniuse.com/" },
+        { text: 'CSS 奇技淫巧', link: 'https://github.com/chokcoco/iCSS' }
+    ]},
+    { text: "Icon", children: [
+        { text: "easyicon", link: "https://www.easyicon.net/" },
+        { text: "iconfont", link: "https://www.iconfont.cn/home/index" },
+        { text: "iconPark", link: "http://iconpark.bytedance.com/" },
+    ]},
+    { text: "图表", children: [
+        { text: "echarts", link: "https://echarts.apache.org/zh/index.html" },
+        { text: "antv", link: "https://antv.vision/zh" },
+    ]},
+    { text: "JS", children: [
+        { text: "ES6入门", link: "https://es6.ruanyifeng.com/" },
+    ]},
+    { text: "TS", children: [
+        { text: "TS文档", link: "https://www.tslang.cn/docs/home.html" },
+        { text: "Handbook", link: "https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/Generics.html" },
+    ]},
+    { text: "React", children: [
+        { text: "Create React App中文文档", link: "https://create-react-app.bootcss.com/" },
+    ]},
+    { text: "Vue", children: [
+        { text: "VueJs", link: "https://cn.vuejs.org/v2/api/" },
+        { text: "Vue Router", link: "https://router.vuejs.org/zh/" },
+        { text: "Vuex", link: "https://vuex.vuejs.org/zh/" },
+        { text: "Vue CLI", link: "https://cli.vuejs.org/zh/" },
+        { text: "VuePress", link: "https://www.vuepress.cn/" },
+        { text: "ElementUI", link: "https://element.eleme.io/#/zh-CN" },
+        { text: "Ant Design Vue", link: "https://www.antdv.com/docs/vue/introduce-cn/" },
+        { text: "Vant", link: "https://youzan.github.io/vant/#/zh-CN/" },
+        { text: "iview", link: "http://v1.iviewui.com/docs/introduce" },
+        { text: "NutUI", link: "https://nutui.jd.com/#/index" },
+    ]},
+    { text: "Angular", children: [
+        { text: "Angular", link: "https://angular.cn/docs" },
+        { text: "Material", link: "https://material.angular.cn/components/categories" },
+        { text: "NG-ZORRO", link: "https://ng.ant.design/components/overview/zh" },
+        { text: "NGRX", link: "https://next.ngrx.io/" },
+    ]},
+    { text: "浏览器", children: [
+        { text: "MDN", link: "https://developer.mozilla.org/zh-CN/" },
+    ]},
+    { text: "打包", children: [
+        { text: "webpack文档", link: "https://www.webpackjs.com/concepts/" },
+        { text: "Rollup", link: "https://www.rollupjs.com/" },
+    ]},
+    { text: "Electron", children: [
+        { text: "Electron", link: "https://www.electronjs.org/docs" },
+    ]},
+    { text: "Node", children: [
+        { text: "Node文档", link: "http://nodejs.cn/api/index.html" },
+        { text: "Deno文档", link: "https://deno.land/" },
+        { text: "Learn Node.js", link: "https://frontendmasters.com/books/front-end-handbook/2018/learning/node.html" },
+        { text: "Express", link: "http://expressjs.jser.us/4x_zh-cn/api.html" },
+        { text: "Express文档翻译", link: "https://www.zybuluo.com/XiangZhou/note/208532" },
+        { text: "Egg", link: "https://eggjs.org/zh-cn/" },
+    ]},
+    { text: "函数式", children: [
+        { text: "Underscore.js", link: "https://underscorejs.bootcss.com/#contains" },
+        { text: "lodash", link: "https://lodash.com/" },
+        { text: "RxJs官方文档", link: "https://cn.rx.js.org/manual/installation.html" },
+        { text: "知乎RxJs专栏", link: "https://zhuanlan.zhihu.com/learing-rxjs" },
+        { text: "RxJs操作符学习", link: "https://rxjs-cn.github.io/learn-rxjs-operators/operators/combination/combineall.html" },
+    ]},
+]};
